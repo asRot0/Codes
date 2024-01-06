75a406538c506ebb067bfb821a53a0be2b49126f
asif <asahmed8890@gmail.com>
2024-01-06 18:24:21 +0600

    creating pandas objects

---
+++ python gfg/Python Pandas/Creating Objects/try3.py
@@ -0,0 +1,46 @@
+# importing pandas
+import pandas as pd
+
+# List of nested dictionary initialization
+list = [{
+    "Student": [{"Exam": 90, "Grade": "a"},
+                {"Exam": 99, "Grade": "b"},
+                {"Exam": 97, "Grade": "c"},
+                ],
+    "Name": "Paras Jain"
+},
+    {
+        "Student": [{"Exam": 89, "Grade": "a"},
+                    {"Exam": 80, "Grade": "b"}
+                    ],
+        "Name": "Chunky Pandey"
+    }
+]
+
+print(list)
+
+# rows list initialization
+rows = []
+
+# appending rows
+for data in list:
+    data_row = data['Student']
+    time = data['Name']
+
+    for row in data_row:
+        row['Name'] = time
+        rows.append(row)
+
+# using data frame
+df = pd.DataFrame(rows)
+
+print(df)
+
+# using pivot_table
+df = df.pivot_table(index='Name', columns=['Grade'],
+                    values=['Exam']).reset_index()
+# Defining columns
+df.columns = ['Name', 'Maths', 'Physics', 'Chemistry']
+
+# print dataframe
+print(df)
---
+++ python gfg/Python Pandas/Creating Objects/try4.py
@@ -0,0 +1,35 @@
+# Import pandas package
+import pandas as pd
+
+# Define a dictionary containing employee data
+data = {'Name': ['Jai', 'Princi', 'Gaurav', 'Anuj'],
+        'Age': [27, 24, 22, 32],
+        'Address': ['Delhi', 'Kanpur', 'Allahabad', 'Kannauj'],
+        'Qualification': ['Msc', 'MA', 'MCA', 'Phd']}
+
+# Convert the dictionary into DataFrame
+df = pd.DataFrame(data)
+
+print(df)
+print('_'*20)
+
+# select two columns
+print(df[['Name', 'Qualification']])
+print('_'*20)
+
+print(df[df.columns[1:]])
+print('_'*20)
+
+
+# using .loc[]
+print(df.loc[0:2, ['Name', 'Age']])
+print('_'*20)
+
+print(df.loc[0, :])
+print('_'*20)
+
+
+# using .iloc[]
+print(df.iloc[0:2, 1:3])
+print('_'*20)
+
