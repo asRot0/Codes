03fbc529c559dc85922edc9479151f4d9b14fadf
asif <asahmed8890@gmail.com>
2024-01-07 20:51:22 +0600

    numpy linear algebra

--- python gfg/Python Numpy/Linear Algebra in NumPy Array/try1.py
+++ python gfg/Python Numpy/Linear Algebra in NumPy Array/try1.py
@@ -19,7 +19,6 @@ print("\nInverse of A:\n", np.linalg.inv(A))
 print("\nMatrix A raised to power 3:\n",
       np.linalg.matrix_power(A, 3))
 
-
 arr = np.array([[1, -2j], [2j, 5]])
 print('array -- ', arr)
 
@@ -28,9 +27,24 @@ c, d = np.linalg.eigh(arr)
 print('eigen value -- ', c)
 print('eigen vector -- ', d)
 
-arr = np.diag((1,2,3))
+arr = np.diag((1, 2, 3))
 print('array -- ', arr)
 
 c, d = np.linalg.eig(arr)
 print('eigen value -- ', c)
 print('eigen vector -- ', d)
+
+m = np.array([[1, 2, 3],
+              [2, 3, 4],
+              [4, 5, 6]])
+
+print("Printing the Original square array:\n", m)
+
+# eigenvalues and eigenvectors
+w, v = np.linalg.eig(m)
+
+# printing eigen values
+print("Printing the Eigen values of the given square array:\n", w)
+
+# printing eigen vectors
+print("Printing Right eigenvectors of the given square array:\n", v)
