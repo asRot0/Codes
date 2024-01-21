98798428803d0ef075725894a693eb0ae6cd17f1
asif <asahmed8890@gmail.com>
2024-01-21 19:13:46 +0600

    CS File

---
+++ C#/MyApplication/classprg.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace MyApplication
+{
+    class classPrg
+    {
+        //public string color = "red my color", color1 = "red";
+        public string color = "redbul my color";
+        public string color1 = "redbul";
+        public static void mainfun()
+        {
+            Console.WriteLine("hello im there!!!");
+        }
+    }
+}
