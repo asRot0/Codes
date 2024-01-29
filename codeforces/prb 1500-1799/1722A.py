ba5d8b66f3f251097da8d0df95ad758d0c895f29
asif <asahmed8890@gmail.com>
2024-01-29 20:00:58 +0600

    Add files via upload

---
+++ codeforces/prb 1500-1799/1722A.py
@@ -0,0 +1,3 @@
+for _ in range(int(input())):
+    n, s = int(input()), input()
+    print(['YES', 'NO'][n!=5 or [*'Timru']!=sorted(s)])
---
+++ codeforces/prb 1500-1799/1722B.py
@@ -0,0 +1,3 @@
+for _ in range(int(input())):
+    _ = input()
+    print('YNEOS'[input().replace('G', 'B')!=input().replace('G', 'B')::2])
