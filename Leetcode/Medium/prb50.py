4f6de26b4ad40f80c23f53f2ec8938574d66f621
asif <asahmed8890@gmail.com>
2024-02-10 11:39:44 +0600

    Pow(x, n)

---
+++ Leetcode/Medium/prb50.py
@@ -0,0 +1,13 @@
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+        if n == 0:
+            return 1
+        if n < 0:
+            x, n = 1 / x, -n
+
+        res = 1
+        while n:
+            if n & 1: res *= x
+            x *= x
+            n //= 2
+        return res
