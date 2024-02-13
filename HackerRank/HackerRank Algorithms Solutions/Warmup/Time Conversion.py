1777a3045c1a7c5d731fac83f5088cb667981924
asif <asahmed8890@gmail.com>
2024-02-13 20:23:55 +0600

    Add files via upload

---
+++ HackerRank/HackerRank Algorithms Solutions/Warmup/Time Conversion.py
@@ -0,0 +1,31 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'timeConversion' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts STRING s as parameter.
+#
+
+def timeConversion(s):
+    # Write your code here
+    h,m,c = map(int, s[:-2].split(':'))
+    p = s[-2:]
+    return '%02d:%02d:%02d' % (h%12+(p=='PM')*12, m, c)
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s = input()
+
+    result = timeConversion(s)
+
+    fptr.write(result + '\n')
+
+    fptr.close()
