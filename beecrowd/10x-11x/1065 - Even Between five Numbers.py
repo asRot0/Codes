9c8d6681e0e952076433930b4d096946c6fd556e
asif <asahmed8890@gmail.com>
2024-03-06 18:54:14 +0600

    Add files via upload

---
+++ beecrowd/10x-11x/1065 - Even Between five Numbers.py
@@ -0,0 +1,6 @@
+li = []
+for i in range(0, 5):
+    a = int(input())
+    if a % 2 == 0:
+        li.append(a)
+print(len(li), 'valores pares')
---
+++ beecrowd/10x-11x/1070 - Six Odd Numbers.py
@@ -0,0 +1,4 @@
+a = int(input())
+for i in range(a, a+12):
+    if i % 2 != 0:
+        print(i)
