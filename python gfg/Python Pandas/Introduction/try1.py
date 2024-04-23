4c6f927ec33f31866899c36ea7cf64abaa112e38
asif <asahmed8890@gmail.com>
2024-04-23 19:09:00 +0600

    python pandas

--- python gfg/Python Pandas/Introduction/try1.py
+++ python gfg/Python Pandas/Introduction/try1.py
@@ -1,7 +1,10 @@
 import pandas as pd
 import numpy as np
 
+ser = pd.Series()
+print("Pandas empty Series: ", ser)
+
 data = np.array(['h', 'e', 'k'])
 
 ser = pd.Series(data)
-print(ser)
+print("Pandas Series:", ser, sep='\n')
