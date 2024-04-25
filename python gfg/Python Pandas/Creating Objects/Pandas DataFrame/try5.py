6b335da162dc12b7c8d0cf6b27a920161527def1
asif <asahmed8890@gmail.com>
2024-04-25 21:56:18 +0600

    Python Pandas DataFrame

--- python gfg/Python Pandas/Creating Objects/Pandas DataFrame/try5.py
+++ python gfg/Python Pandas/Creating Objects/Pandas DataFrame/try5.py
@@ -2,7 +2,10 @@ import pandas as pd
 
 
 # making data frame from csv file
-data = pd.read_csv("../nba.csv", index_col="Name")
+data = pd.read_csv("../../nba.csv", index_col="Name")
+
+print(data)
+print('_'*30)
 
 # retrieving row by loc method
 d1 = data.loc["Avery Bradley"]
@@ -15,12 +18,12 @@ print('_'*30)
 print(data.loc[["Nerlens Noel", "Amir Johnson"]])
 print('_'*30)
 
-# print(data.head(5))
-# print(data.drop(["Avery Bradley", "John Holland", "R.J. Hunter"], inplace=True))
+print(data.head(5))
+
+# dropping passed values
+print(data.drop(["Avery Bradley", "John Holland", "R.J. Hunter"], inplace=True))
 print(data.head(10))
 
 # dropping passed columns
 data.drop(["Team", "Weight", "College"], axis=1, inplace=True)
-
-# display
 print(data.head())
