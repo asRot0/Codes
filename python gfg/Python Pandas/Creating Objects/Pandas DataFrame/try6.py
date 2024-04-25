885c4fc0d7a0b0968e5bad5531959284b6c0a0de
asif <asahmed8890@gmail.com>
2024-04-25 22:29:15 +0600

    Python Pandas DataFrame

---
+++ python gfg/Python Pandas/Creating Objects/Pandas DataFrame/try6.py
@@ -0,0 +1,26 @@
+import pandas as pd
+
+data = pd.read_csv("../../nba.csv")
+data.index += 1
+
+print(data)
+print('_' * 20)
+
+print(data.tail())
+print('_' * 20)
+
+print(data.head(10))
+print('_' * 20)
+
+# to add a row in dataframe
+# concat the old dataframe with new one
+
+new_row = pd.DataFrame({'Name': 'Joe', 'Team': 'Boston', 'Number': 3,
+                        'Position': 'PG', 'Age': 34, 'Height': '6-2',
+                        'Weight': 189, 'College': 'MIT', 'Salary': 1128920},
+                       index=[0])
+
+# simply concatenate both dataframes
+data = pd.concat([new_row, data]).reset_index(drop=True)  # drop=False
+data.index += 1
+print(data.head())
