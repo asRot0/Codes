336c15b3eecba4806d2fadf23061d2da19767dc2
asif <asahmed8890@gmail.com>
2024-05-06 19:56:37 +0600

    Python Pandas DataFrame

--- python gfg/Python Pandas/Creating Objects/Pandas DataFrame/try7.py
+++ python gfg/Python Pandas/Creating Objects/Pandas DataFrame/try7.py
@@ -1,7 +1,16 @@
 import pandas as pd
 
-data = pd.read_csv('../../nba.csv', index_col='Name')
-
-first = data['Age']
-print(first)
+data = pd.read_csv('../../nba.csv')
+print(data.head())
 print('_'*20)
+
+data.sort_values('Team', inplace=True)
+condition1 = data['Team'] == 'Atlanta Hawks'
+
+data.where(condition1, inplace=True)
+print(data.head(10))
+print('_'*20)
+
+condition2 = data['Age'] > 24
+data.where(condition1 & condition2, inplace=True)
+print(data.head())
