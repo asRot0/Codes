7330948cdc0c96783e0cd8aea2319a49cc5da54d
asif <asahmed8890@gmail.com>
2024-08-30 00:01:11 +0600

    Remove Linked List Elements

---
+++ Leetcode/Easy/prb203.py
@@ -0,0 +1,21 @@
+from typing import Optional
+
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def removeElements(self, head: Optional[ListNode], val: int) -> Optional[ListNode]:
+        temp = ListNode(next=head)
+        curr = temp
+
+        while curr.next:
+            if curr.next.val == val:
+                curr.next = curr.next.next
+            else:
+                curr = curr.next
+        return temp.next
