8cc3a8d6404086e5cd0634c665a34e193dea2162
asif <asahmed8890@gmail.com>
2024-10-10 19:03:11 +0600

    Add files via upload

--- codeforces/prb 2000- 2200/2009C.py
+++ codeforces/prb 2000- 2200/2009C.py
@@ -2,6 +2,6 @@ import math
 for _ in range(int(input())):
     x, y, k = map(int, input().split())
     if math.ceil(x/k) <= math.ceil(y/k):
-        print(2 * math.ceil(y/k))
+        print(2 * math.ceil(y/k))  # y_jumps = ⌈y/k⌉ can be computed as (y + k - 1) // k
     else:
-        print(2 * math.ceil(x/k)-1)
+        print(2 * math.ceil(x/k)-1)  # x_jumps = ⌈x/k⌉ can be computed as (x + k - 1) // k
