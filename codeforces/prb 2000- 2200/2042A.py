60d145e1592ec18906b330d3e945f2eec2f72456
asif <asahmed8890@gmail.com>
2024-12-13 19:04:17 +0600

    Add files via upload

--- codeforces/prb 2000- 2200/2042A.py
+++ codeforces/prb 2000- 2200/2042A.py
@@ -1,13 +1,9 @@
 for _ in range(int(input())):
     _, k = map(int, input().split())
-    a = list(map(int, input().split()))
-    a.sort(reverse=True)
-    for i in a:
-        if k==i:
-            print(0)
+    for i in sorted(map(int, input().split()), reverse=True):
+        if k <= i:
+            print(0 if k == i else k)
             break
-        elif k<i:
-            print(k)
-            break
-        else: k -= i
-    else: print(k)
+        k -= i
+    else:
+        print(k)
