9b2c9a6574a28a1491fdc333508cea4ff7943ccf
asif <asahmed8890@gmail.com>
2024-12-18 19:39:47 +0600

    Add files via upload

---
+++ codeforces/prb 2000- 2200/2038j.py
@@ -0,0 +1,14 @@
+b = p = 0
+for _ in range(int(input())):
+    e, a = input().split()
+    if e == 'B':
+        b += int(a)
+        if b > p:
+            print('yes')
+            b = p = 0
+        else:
+            p -= b
+            b = 0
+            print('no')
+    else:
+        p += a
