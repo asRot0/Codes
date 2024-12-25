aef25896701a0916288b2456d074a2591d9de81c
asif <asahmed8890@gmail.com>
2024-12-25 19:51:58 +0600

    Add files via upload

---
+++ codeforces/prb 2000- 2200/2050B.py
@@ -0,0 +1,19 @@
+for _ in range(int(input())):
+    n = int(input())
+    a = list(map(int, input().split()))
+    c = sum(a) // n
+    if sum(a) % n:
+        print('no')
+
+    else:
+        for i in range(1, n-1):
+            if a[i-1]>c:
+                r = a[i-1] - c
+                a[i-1] -= r
+                a[i+1] += r
+            elif a[i-1]<c:
+                r = c - a[i-1]
+                a[i-1] += r
+                a[i+1] -= r
+        # print(a)
+        print(['no','yes'][len(set(a))==1])
